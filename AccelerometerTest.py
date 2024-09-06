e5f0d5b1880a24c56e3733c3e9de629e9a9b498a
Anthony Cieri <penguinmillion@gmail.com>
2024-09-05 20:30:55 -0700

    feat: Accelerometer test script

---
+++ AccelerometerTest.py
@@ -0,0 +1,16 @@
+
+#! /bin/python3
+
+import bluerobotics_navigator as brn
+import numpy as np
+
+brn.init()
+
+while True:
+
+    acc = brn.read_accel()
+    acc_mag = (acc.x * acc.x + acc.y * acc.y + acc.z * acc.z) ** 0.5
+    acc_dir = (acc.x / acc_mag, acc.y / acc_mag, acc.z / acc_mag)
+    angle = np.acos(acc_dir[2])
+
+    print(f"Acceleration: ({acc.x:10.5f}, {acc.y:10.5f}, {acc.z:10.5f}), angle: {angle:10.5f}")
