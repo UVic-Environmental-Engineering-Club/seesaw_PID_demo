4b1c8f940fd51c25301f82914f6693477b41fc3e
Anthony Cieri <penguinmillion@gmail.com>
2024-09-05 20:45:32 -0700

    chore: Documentation

--- AccelerometerTest.py
+++ AccelerometerTest.py
@@ -8,9 +8,18 @@ brn.init()
 
 while True:
 
+    # Get acceleration from the NFC, note that positive z is down
     acc = brn.read_accel()
+
+    # Normalize the acceleration vector
     acc_mag = (acc.x * acc.x + acc.y * acc.y + acc.z * acc.z) ** 0.5
     acc_dir = (acc.x / acc_mag, acc.y / acc_mag, acc.z / acc_mag)
+
+    # Use the dot product angle formula to get the angle (in radians) of acceleration off of (0, 0, 1)
+    # (what it would be if the NFC was level)
     angle = np.acos(acc_dir[2])
+
+    # TODO: Calculate angles off of a known axis, e.g. North,
+    # TODO: or something set when the script is started
 
     print(f"Acceleration: ({acc.x:10.5f}, {acc.y:10.5f}, {acc.z:10.5f}), angle: {angle:10.5f}")
